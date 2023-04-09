dbf7bc2b2d4b1c9579d90a3f643562b4a25772a7
Bryan Wilhite <bryan.wilhite@gmail.com>
2023-04-09 12:42:04 -0700

    #3 🔨

--- Songhay.Player.ProgressiveAudio/Components/PlayerElmishComponent.fs
+++ Songhay.Player.ProgressiveAudio/Components/PlayerElmishComponent.fs
@@ -1,5 +1,8 @@
 namespace Songhay.Player.ProgressiveAudio.Components
 
+open Microsoft.AspNetCore.Components
+open Microsoft.JSInterop
+
 open Bolero
 open Bolero.Html
 
@@ -7,6 +10,12 @@ open Songhay.Player.ProgressiveAudio.Models
 
 type PlayerElmishComponent() =
     inherit ElmishComponent<ProgressiveAudioModel, ProgressiveAudioMessage>()
+
+    static member EComp model dispatch =
+        ecomp<PlayerElmishComponent, _, _> model dispatch { attr.empty() }
+
+    [<Inject>]
+    member val JSRuntime = Unchecked.defaultof<IJSRuntime> with get, set
 
     override this.View model dispatch =
         section {
