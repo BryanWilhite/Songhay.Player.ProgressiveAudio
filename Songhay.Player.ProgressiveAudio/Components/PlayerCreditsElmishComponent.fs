45b46b0166a0beebf7b1ad5a68a3152e559fe1ca
Bryan Wilhite <bryan.wilhite@gmail.com>
2023-05-03 21:18:32 -0700

    #3 🔨🍱💄

--- Songhay.Player.ProgressiveAudio/Components/PlayerCreditsElmishComponent.fs
+++ Songhay.Player.ProgressiveAudio/Components/PlayerCreditsElmishComponent.fs
@@ -7,23 +7,26 @@ open Bolero
 open Bolero.Html
 
 open Songhay.Modules.Bolero.Models
+open Songhay.Modules.Bolero.Visuals.Bulma
 open Songhay.Player.ProgressiveAudio.Models
 
-type PlayerElmishComponent() =
+type PlayerCreditsElmishComponent() =
     inherit ElmishComponent<ProgressiveAudioModel, ProgressiveAudioMessage>()
 
-    let sectionNode (_: IJSRuntime) model dispatch =
-        section {
-            [ "player"; "progressive-audio" ] |> CssClasses.toHtmlClassFromList
+    let buttonNode (_: IJSRuntime) model dispatch =
+        button {
+            [ "credits" ] |> CssClasses.toHtmlClassFromList
 
-            (model, dispatch) ||> PlayerCreditsElmishComponent.EComp
+            span {
+                empty()
+            }
         }
 
     static member EComp model dispatch =
-        ecomp<PlayerElmishComponent, _, _> model dispatch { attr.empty() }
+        ecomp<PlayerCreditsElmishComponent, _, _> model dispatch { attr.empty() }
 
     [<Inject>]
     member val JSRuntime = Unchecked.defaultof<IJSRuntime> with get, set
 
     override this.View model dispatch =
-        (model, dispatch) ||> sectionNode this.JSRuntime
+        (model, dispatch) ||> buttonNode this.JSRuntime
