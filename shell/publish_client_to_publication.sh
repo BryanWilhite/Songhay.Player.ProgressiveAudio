df9d7dd8454a1b9ead8831962a8c1277e58b950c
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-08-20 22:34:59 -0700

    test [[webpack]] pipelines ✋👓

--- shell/publish_client_to_publication.sh
+++ shell/publish_client_to_publication.sh
@@ -41,8 +41,6 @@ rsync -r --delete-after \
     --stats \
     "$rsync_from" "$rsync_to"
 
-exit 0
-
 echo "deleting any existing files at publish target..."
 
 rm -rf "../$client_project_name/bin/Release/net10.0/publish"
